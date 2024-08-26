6511db77bb57bb58c6b829e13ae8d1dd0b68f48f
Domala Sherpa <domalasherpa9813@gmail.com>
2024-08-26 12:21:02 +0545

    refactor: added simple greet function

--- test.js
+++ test.js
@@ -1,4 +1,7 @@
-
-function greet(){
-    console.log("this is the day 1: ");
+function greet() {
+  console.log("this is the day 1: ");
 }
+
+const anotherGreet = () => {
+  console.log("Bonjour.");
+};
