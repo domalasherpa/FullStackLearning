60ec811ebb33e412511680e298454bd7f2b2ecd3
domalasherpa <domalasherpa9813@gmail.com>
2024-09-08 14:25:19 +0545

    feat: shadow on hover and link in div

--- cart/src/components/productSingle.vue
+++ cart/src/components/productSingle.vue
@@ -1,26 +1,23 @@
 <script setup>
-
-
 const props = defineProps({
     product: Object
 });
-
-const productLink = '/products/' + props.product.id;
-
 </script>
 
 <template>
-    <div class="bg-white dark:bg-gray-800  cursor-pointer flex-col h-full justify-between">
-        <div class="h-40 border-2 border-orange-200 shadow">
-            <img class="p-2 h-full w-full object-contain" :src="product.image" :alt="product.title" />
+    <router-link :to="{name: 'productDetails', params:{id: product.id}}">
+        <div class="bg-white cursor-pointer flex-col h-full justify-between hover:shadow-md">
+            <div class="h-40 border-2 border-orange-200 shadow">
+                <img class="p-2 h-full w-full object-contain" :src="product.image" :alt="product.title" />
+            </div>
+            <div class="px-2">
+                
+                    <h5 class="text-xs font-semibold tracking-tight text-gray-900">{{ product.title }}</h5>
+                
+                <p class="text-xs my-2 font-bold text-orange-600"><span>Rs</span>{{ product.price }}</p>
+            </div>
         </div>
-        <div class="my-4">
-            <a :href="productLink">
-                <h5 class="text-xs font-semibold tracking-tight text-gray-900 dark:text-white">{{ product.title }}</h5>
-            </a>
-            <p class="text-xs my-2 font-bold text-orange-600"><span>Rs</span>{{ product.price }}</p>
-        </div>
-    </div>
+    </router-link>
 </template>
 
 
