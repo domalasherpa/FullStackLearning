1bcdd0a54fcac364f476b858bfa20026d9121759
domalasherpa <domalasherpa9813@gmail.com>
2024-09-09 12:22:25 +0545

    feat: removal of unnecessary files

--- cart/src/App.vue
+++ cart/src/App.vue
@@ -1,15 +1,5 @@
 <script setup>
-import { ref, computed } from "vue";
 import { RouterView } from "vue-router";
-
-import productsPage from "./pages/productsPage.vue";
-import cartPage from './pages/cartPage.vue';
-import homePage from "./pages/homePage.vue";
-import productSingle from "./components/productSingle.vue";
-import notFound from "./pages/notFound.vue";
-
-
-//test
 import navBar from "./components/navBar.vue";
 </script>
 
