fcd44d706609089b7daa65e373b01ff98d5a8795
domalasherpa <domalasherpa9813@gmail.com>
2024-09-10 16:54:46 +0545

    feat: adds checkout route path

--- cart/src/routes/rootRoute.js
+++ cart/src/routes/rootRoute.js
@@ -4,12 +4,14 @@ import Home from "../pages/HomePage.vue";
 import Products from "../pages/ProductsPage.vue";
 import Product from "../pages/ProductPage.vue";
 import NotFound from "../pages/NotFound.vue";
+import Checkout from "../pages/CheckoutPage.vue";
 
 const routes = [
     {path:'/', name:'home', component: Home},
     {path:'/products', name:'products', component: Products, name: 'products'},
     {path:'/cart', name:'cart', component: Cart},
-    {path:'/products/:id', name:'productDetails', component: Product}
+    {path:'/products/:id', name:'productDetails', component: Product},
+    {path:'/checkout', name:'checkout', component: Checkout}
 ]
 
 const router = createRouter({
