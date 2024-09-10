cd237bb240f52ade70012580bb35ef753beb7e39
domalasherpa <domalasherpa9813@gmail.com>
2024-09-10 05:54:13 +0545

    fix: adds quantity update in cart and select option

--- cart/src/pages/CartPage.vue
+++ cart/src/pages/CartPage.vue
@@ -1,112 +1,139 @@
 <script setup>
-import { carts } from '../data/data';
-import { computed, ref, toDisplayString, watch } from "vue";
+import { computed, reactive, ref } from 'vue';
+import { carts, products } from "../data/data";
 
-//todo make selecteditems a global store to access from the checkout.
-const selectedItemsIndex = ref([]);
-const total = ref(0);
+const selectedItems = reactive({});
 const allSelected = ref(false);
 
-const selectedItems = computed(()=>{
-    return selectedItemsIndex?.value.map(i=>carts.value[i])
+const isCartEmpty = computed(() => {
+	return Object.entries(carts).length === 0;
 });
 
-const isAllSelected = computed(() => {
-  return selectedItemsIndex.value.length === carts.value.length;
+const total = computed(() => {
+	const temp = Object.values(selectedItems).reduce((acc, item) => acc + item.quantity * item.price, 0);
+	return parseFloat(temp.toFixed(2));
 });
 
-watch(allSelected, (newVal)=>{
-    if(newVal){
-        selectedItemsIndex.value = Array.from(carts.value.keys());//to set all the index selected
-    }
-    else{
-        selectedItemsIndex.value = []
-    }
-}, {immediate: true});
+const selectAll = (event) => {
+	if (event.target.checked) {
+		Object.keys(carts).forEach(productId => {
+			selectedItems[productId] = carts[productId];
+		});
+	} else {
+		Object.keys(selectedItems).forEach(productId => {
+			delete selectedItems[productId];
+		});
+	}
+	allSelected.value = event.target.checked;
+};
 
-watch(selectedItems, ()=>{
-    const unTranformedTotal = selectedItems?.value.reduce((total, item)=>total+item.price*item.quantity, 0);
-    total.value = parseFloat(unTranformedTotal.toFixed(2));
-}, {immediate: true});
+// if all are not selected or if one unselected then change state of allSelected
+const updateAllSelectedState = () => {
+	const totalItems = Object.keys(carts).length;
+	const selectedItemsCount = Object.keys(selectedItems).length;
+	allSelected.value = totalItems === selectedItemsCount;
+};
 
-const removeProduct= (index)=>{
-    carts.value.splice(index, 1);
-    //this is to ensure that the index of the items remains unchanged.[0, 1, 2, 3, 4, 5, 6] , to remove 2, => [0, 1] + [3, 4, 5, 6]
-    selectedItemsIndex.value = selectedItemsIndex.value.filter(i => i < index).concat(selectedItemsIndex.value.filter(i => i > index).map(i => i - 1));
+const handleCheckboxToggle = (productId) => {
+	if (selectedItems[productId]) {
+		delete selectedItems[productId];
+	} else {
+		selectedItems[productId] = carts[productId];
+	}
+	updateAllSelectedState();
+};
+
+const removeProduct = (productId) => {
+	delete carts[productId];
+	delete selectedItems[productId];
+	updateAllSelectedState();
+};
+
+const handleCheckout = () => { //check for condn and route-link
+	alert('Proceeding to checkout');
+
+};
+
+const handleQuantityDcr = (productId) => {
+	if (carts[productId].quantity > 1) {
+		carts[productId].quantity -= 1;
+	}
+};
+
+const handleQuantityInc = (productId) => {
+	if (carts[productId].quantity < carts[productId].availableQuantity) { //replace the available quanity with dynamic data
+		carts[productId].quantity += 1;
+	} else {
+		alert("Please choose up to the available quantity only.");
+	}
+};
+
+const handleInputQuantity = (event, productId) => {
+	const inputQuantity = parseInt(event.target.value, 10);
+	if (isNaN(inputQuantity) || inputQuantity < 1 || inputQuantity > carts[productId].availableQuantity) {
+
+		alert("please enter a valid quantity");
+	} else {
+		carts[productId].quantity = inputQuantity;
+	}
 }
-
-const handleCheckout = ()=>{
-    if(selectedItems.length < 1){
-        alert("please select items.");
-    }
-    else{
-        alert("Checked out");
-        //send users to the checkout page. & handle checkout logic
-        selectedItemsIndex.value.forEach(index=>{
-            carts.value = carts.value.splice(index, 1);
-        })
-    }
-}
-
 </script>
 
 <template>
-    <div class="w-full flex flex-col items-center mb-8">
-        <h1 class="text-xl text-gray-600 font-bold my-2">My Cart</h1>
-        <table class="w-9/12 text-sm text-left rtl:text-right text-gray-500">
-            <thead class="text-xs text-gray-700 uppercase bg-gray-50">
-                <tr>
-                    <th scope="col" class="px-2 py-3">
-                        <input type="checkbox" class="cursor-pointer" v-model="allSelected" :checked="isAllSelected">
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        <span class="sr-only">Image</span>
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Product
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Qty
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Price
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Action
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr class="bg-white border-b hover:bg-gray-50 text-xs" v-for="(product, index) in carts" :key="index">
-                    <td class="px-2 py-4">
-                        <input type="checkbox" v-model="selectedItemsIndex" :value="index" class="cursor-pointer" :checked="allSelected" :key="index"/>
-                    </td>
-                    <td class="p-4">
-                        <img :src="product.image" class="md:w-16 max-w-full max-h-full" :alt="product.title">
-                    </td>
-                    <td class="px-6 py-4 font-semibold  text-gray-900">
-                        {{ product.title }}
-                    </td>
-                    <td class="px-6 py-4 font-semibold">
-                        {{ product.quantity }}
-                    </td>
-                    <td class="px-6 py-4 font-semibold text-orange-500">
-                        {{ product.price }}
-                    </td>
-                    <td class="px-6 py-4">
-                        <i class="fa fa-trash cursor-pointer" aria-hidden="true" @click="removeProduct(index)"></i>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-    <div class="flex gap-4 items-center">
-        <div class="space-x-2">
-            <span class="text-gray-600 font-bold">Subtotal:</span>
-            <span class="font-semibold text-orange-500">Rs. {{ total }}</span>
-        </div>
-        <button class="bg-orange-500 px-4 py-1 rounded-sm text-white font-semibold" @click="handleCheckout">Check Out</button>
-    </div>
+	<div v-if="isCartEmpty">
+		<p>Whoops! your cart is empty.</p>
+	</div>
+	<div v-else class="w-full flex flex-col items-center mb-8">
+		<h1 class="text-xl text-gray-600 font-bold my-2">My Cart</h1>
+		<table class="w-9/12 text-sm text-left rtl:text-right text-gray-500">
+			<thead class="text-xs text-gray-700 uppercase bg-gray-50">
+				<tr>
+					<th scope="col" class="px-2 py-3">
+						<input type="checkbox" class="cursor-pointer" :checked="allSelected" @change="selectAll" />
+					</th>
+					<th scope="col" class="px-6 py-3">
+						<span class="sr-only">Image</span>
+					</th>
+					<th scope="col" class="px-6 py-3">Product</th>
+					<th scope="col" class="px-6 py-3">Qty</th>
+					<th scope="col" class="px-6 py-3">Price</th>
+					<th scope="col" class="px-6 py-3">Action</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr v-for="(product) in carts" :key="product.id" class="bg-white border-b hover:bg-gray-50 text-xs">
+					<td class="px-2 py-4">
+						<input type="checkbox" :value="product.id" class="cursor-pointer"
+							:checked="product.id in selectedItems" @change="handleCheckboxToggle(product.id)" />
+					</td>
+					<td class="p-4">
+						<img :src="product.image" class="md:w-16 max-w-full max-h-full" :alt="product.title">
+					</td>
+					<td class="px-6 py-4 font-semibold text-gray-900">{{ product.title }}</td>
+					<td class="px-6 py-4 font-semibold">
+						<div class="space-x-4">
+							<button @click="handleQuantityDcr(product.id)">-</button>
+							<input class="w-10 text-center border-2 border-gray-200" type="text"
+								:value="product.quantity" @change="handleInputQuantity($event, product.id)"
+								placeholder="1" />
+							<button @click="handleQuantityInc(product.id)">+</button>
+						</div>
+					</td>
+					<td class="px-6 py-4 font-semibold text-orange-500">{{ product.price }}</td>
+					<td class="px-6 py-4">
+						<i class="fa fa-trash cursor-pointer" aria-hidden="true" @click="removeProduct(product.id)"></i>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+		<div class="flex gap-4 items-center">
+			<div class="space-x-2">
+				<span class="text-gray-600 font-bold">Subtotal:</span>
+				<span class="font-semibold text-orange-500">Rs. {{ total }}</span>
+			</div>
+			<button class="bg-orange-500 px-4 py-1 rounded-sm text-white font-semibold" @click="handleCheckout">
+				Check Out
+			</button>
+		</div>
+	</div>
 </template>
-
-<style scoped></style>
